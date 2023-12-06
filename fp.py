fc2a923b86be535e7cf84e7c15a9a8af4c1d8398
Theotrgl <91537576+Theotrgl@users.noreply.github.com>
2023-12-06 13:02:11 +0700

    update logic to include flask

--- fp.py
+++ fp.py
@@ -6,6 +6,57 @@ from bs4 import BeautifulSoup
 import os
 
 exit = False
+def amino_acid_to_codon():
+    codon_table = {
+        'A': ['GCU', 'GCC', 'GCA', 'GCG'],
+        'R': ['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],
+        'N': ['AAU', 'AAC'],
+        'D': ['GAU', 'GAC'],
+        'C': ['UGU', 'UGC'],
+        'Q': ['CAA', 'CAG'],
+        'E': ['GAA', 'GAG'],
+        'G': ['GGU', 'GGC', 'GGA', 'GGG'],
+        'H': ['CAU', 'CAC'],
+        'I': ['AUU', 'AUC', 'AUA'],
+        'L': ['UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'],
+        'K': ['AAA', 'AAG'],
+        'M': ['AUG'],
+        'F': ['UUU', 'UUC'],
+        'P': ['CCU', 'CCC', 'CCA', 'CCG'],
+        'S': ['UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'],
+        'T': ['ACU', 'ACC', 'ACA', 'ACG'],
+        'W': ['UGG'],
+        'Y': ['UAU', 'UAC'],
+        'V': ['GUU', 'GUC', 'GUA', 'GUG'],
+        '*': ['UAA', 'UAG', 'UGA']  # Stop codons
+    }
+    return codon_table
+
+def count_codons(sequence):
+    codon_table = amino_acid_to_codon()  # Use the previously defined amino_acid_to_codon function
+
+    codon_count = {}
+    for amino_acid in sequence:
+        possible_codons = codon_table.get(amino_acid, [])
+        if possible_codons:
+            # Counting the codons
+            for codon in possible_codons:
+                if codon in codon_count:
+                    codon_count[codon] += 1
+                else:
+                    codon_count[codon] = 1
+    
+    return codon_count
+def amino_acid_sequence_to_codons(sequence):
+    codon_table = amino_acid_to_codon()
+    codon_sequence = []
+
+    for amino_acid in sequence:
+        possible_codons = codon_table.get(amino_acid, [])
+        if possible_codons:
+            codon_sequence.append(possible_codons[0])  # Choosing the first codon for simplicity
+    
+    return codon_sequence
 
 def calculate_molecular_mass(structure):
     atomic_weights = {
@@ -34,6 +85,8 @@ def extract_amino_acid_sequence(structure):
 
     return ''.join(amino_acids)
 
+def search_codon(codon_counts, codon):
+    return codon_counts.get(codon, 0)
 def main():
     global exit
     folder_path = "./PDB_Files"
@@ -55,29 +108,46 @@ def main():
                 structure = parser.get_structure(pdb_id.upper(), file_path)
                 molecular_weight = calculate_molecular_mass(structure)
                 amino_acid = extract_amino_acid_sequence(structure)
-                print("Molecular Weight: ", molecular_weight)
-                print("Amino Acid: ", amino_acid)
-                # Save the visualization to an HTML file
+                codons = amino_acid_sequence_to_codons(amino_acid)
+                codon_count = count_codons(amino_acid)
                 view.add_component(file_path)  # Add the component again to ensure it's loaded
-                # view._set_size("600px", "400px")  # Set size (optional)
                 # Export the widget as an HTML file
-                filename = "protein_structure.html"
+                filename = f"{pdb_id.lower()}_protein_structure.html"
                 nv.write_html(filename, [view])
                 print(f"Visualization saved to {filename}")
-
+                # Save the visualization to an HTML file
                 with open(filename, 'r') as file:
                     html_content = file.read()
 
                 soup = BeautifulSoup(html_content, 'html.parser')
-
+                new_div = soup.new_tag('div')
+                new_div['class'] = 'new-component'
+                form_tag = soup.new_tag('form', action='/', method='post')
+                input_tag = soup.new_tag('input')
+                button_tag = soup.new_tag('button', type='submit')
+                button_tag.string = 'Submit'
+                # new_p = soup.new_tag('p')
+                # new_p['class'] = 'test'
+                # new_p.string = '{{amino_acid}}'
+                # new_div.string = f'Molecular Weight: {molecular_weight}\nAmino Acid Structure: {amino_acid}\nCodon Count: {codon_count}'
                 title_element = soup.find(class_='title')
                 if title_element:
                     title_element.string = 'New Title'
-
+                existing_element = soup.find('body')
+                if existing_element:
+                    existing_element.append(new_div)
+                    existing_element.append(form_tag)
+                elem = soup.find(class_ = 'new-component')
+                if elem:
+                    elem.append(input_tag)
+                    elem.append(button_tag)
+                # find_new_div = soup.find(class_ = 'new-component')
+                # if find_new_div:
+                #     find_new_div.append(new_p)
                 # Save the modified content back to the file
-                with open('new_html_file.html', 'w') as file:
+                with open(f'new-{pdb_id.lower()}_html_file.html', 'w') as file:
                     file.write(str(soup))
-
+                
                 if os.path.exists(filename):
                     os.remove(filename)
                     print(f"{filename} has been deleted.")
---
+++ test.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Handle the form submission here
+        submitted_data = request.form['input_name']  # Change 'input_name' to the actual name of your input field
+        # Process the submitted data or perform any required actions
+        print("Submitted data:", submitted_data)  # You can perform any action with the submitted data here
+        return "Form submitted successfully!"  
+
+    return render_template('new-1a1o_html_file.html')  
+
+if __name__ == '__main__':
+    app.run(debug=True)
